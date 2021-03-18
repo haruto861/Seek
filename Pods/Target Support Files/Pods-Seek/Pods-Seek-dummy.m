e79a1eed666506fe1c17f73d65be51cb0a685693
haruto <75295738+haruto861@users.noreply.github.com>
2021-03-18 20:14:45 +0900

    Pods / xcuserdata / .DS_Store の追跡停止

--- Pods/Target Support Files/Pods-Seek/Pods-Seek-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Seek : NSObject
-@end
-@implementation PodsDummy_Pods_Seek
-@end
--- Pods/Target Support Files/Pods-SeekTests/Pods-SeekTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SeekTests : NSObject
-@end
-@implementation PodsDummy_Pods_SeekTests
-@end
