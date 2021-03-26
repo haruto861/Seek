2c4bf172d497a0fe4bb2dfdf01957cac419c088d
haruto <75295738+haruto861@users.noreply.github.com>
2021-03-26 20:04:36 +0900

    slackで質問した箇所のPR

--- Seek/Cells/PostTableViewCell.swift
+++ Seek/Cells/PostTableViewCell.swift
@@ -10,8 +10,16 @@ import UIKit
 class PostTableViewCell: UITableViewCell {
     @IBOutlet var passedCustomizeLabel: UILabel!
     @IBOutlet var passeMenuImage: UIImageView!
+    var customizeMenuNames =  [String]()
+    var customizeArray = [Customize]()
+    var indexPath:IndexPath?
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    override func layoutSubviews() {
+        if customizeArray.count > 0  {
+            print(customizeArray)
+        }
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
--- Seek/Controllers/PostViewController.swift
+++ Seek/Controllers/PostViewController.swift
@@ -28,54 +28,69 @@ class PostViewController: UIViewController, UITableViewDelegate, UITableViewData
     var menuNames = [String]()
     var menuImages = [String]()
 
+    var customizeArrays = [Customize]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         customizesTableView.rowHeight = 70
         customizesTableView.delegate = self
         customizesTableView.dataSource = self
-        let nib = UINib(nibName: "PostTableViewCell", bundle: Bundle.main)
-        customizesTableView.register(nib, forCellReuseIdentifier: "postCell")
-        getUserDefaultsData()
-        getSelectedMenuData()
-        getSeletedCustomizeData()
-    }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return customizeMenuNames.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as? PostTableViewCell else { abort()}
-        cell.passedCustomizeLabel.text = customizeMenuNames[indexPath.row]
-        return cell
-    }
-
-    private func getUserDefaultsData() {
         let ud = UserDefaults.standard
         if ud.array(forKey: "choice") == nil {
         } else {
             selectedCustomizes = ud.array(forKey: "choice") as? [String] ?? []
+            print(selectedCustomizes)
         }
         selectedMenu = ud.string(forKey: "selected")
         menuCalorie += ud.integer(forKey: "menuCalorie")
         menuPrice += ud.integer(forKey: "menuPrice")
+        getSelectedMenuData()
+        getSeletedCustomizeData()
 
+        let nib = UINib(nibName: "PostTableViewCell", bundle: Bundle.main)
+        customizesTableView.register(nib, forCellReuseIdentifier: "postCell")
     }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return customizeArrays.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as? PostTableViewCell else { abort()}
+        print(customizeArrays)
+        if customizeArrays.count > 0 {
+        }
+        return cell
+    }
+
 
     private func getSeletedCustomizeData() {
         let query = NCMBQuery(className: "customize")
+        print(selectedCustomizes) // userdefaultsより先に呼ばれるとデータが空になる
         query?.whereKey("customize", containedIn: selectedCustomizes)
         query?.findObjectsInBackground({ (results, error) in
+            print(results)
             guard let customizes = results as? [NCMBObject] else { return }
             for customize in customizes {
                 guard let selectedCustomize = customize.object(forKey: "customize") as? String else { return }
+                print(selectedCustomize)
                 guard let selectedCustomizeImageUrl = customize.object(forKey: "image") as? String else { return }
+                print(selectedCustomizeImageUrl)
                 guard let selectedCustomizePrice = customize.object(forKey: "price") as? Int else { return  }
+                print(selectedCustomizePrice)
                 guard let selectedCustomizeCalorie = customize.object(forKey: "calorie") as? Int else { return  }
+                print(selectedCustomizeCalorie)
+                let customizeArray  = Customize(customizeName: selectedCustomize, customizeImageUrl: selectedCustomizeImageUrl, customizePrice: selectedCustomizePrice, customizeCalorie: selectedCustomizeCalorie)
+                print(customizeArray)
+                self.customizeArrays.append(customizeArray)
+                print(self.customizeArrays)
                 self.customizePrice += selectedCustomizePrice
                 self.customizeCalorie += selectedCustomizeCalorie
-                self.customizeMenuNames.append(selectedCustomize)
-                self.customImageUrls.append(selectedCustomizeImageUrl)
+//                self.customizeMenuNames.append(selectedCustomize)
+//                self.customImageUrls.append(selectedCustomizeImageUrl)
             }
         })
         self.customizesTableView.reloadData()
